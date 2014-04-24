54c01ea7bc02188b58396323f471b3f1153dcd62
Jonathan Creasy <jonathan@ghostlab.net>
2014-04-24 16:00:58 -0700

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,6 +7,33 @@ node 'mysql01.ghostlab.net' {
     ensure => installed,
   }
 
+  class { '::mysql::server':
+    root_password    => 'strongpassword',
+    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  }
+ 
+  mysql_user { 'oouser@%':
+    ensure                   => 'present',
+    max_connections_per_hour => '0',
+    max_queries_per_hour     => '0',
+    max_updates_per_hour     => '0',
+    max_user_connections     => '0',
+  }
+
+  mysql_grant { 'oouser@%/oodb.*':
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table      => '*.*',
+    user       => 'oouser@%',
+  }
+
+  mysql_database { 'oodb':
+    ensure  => 'present',
+    charset => 'utf8',
+    collate => 'utf8_swedish_ci',
+  }
+  
   firewall { '3360 accept MySQL Traffic':
     proto   => 'tcp',
     port    => '3360',
